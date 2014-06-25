6b9af27660611cf33db0e13feaf81cb9001a0eae
pierrelemarchand <pierrelemarchand@MacBook-Pro-de-Pierre.local>
2014-06-25 11:34:47 -0700

    folder suppressed

--- first_envelope/temp_script.bash
+++
@@ -1,4 +0,0 @@
-
-# Calculating vertical illuminance of ground (array)
-vwrays -x 10 -y 10 -vf first_envelope/down_l.vf 	| rcalc -e '$1=$1;$2=$2;$3=100;$4=$4;$5=$5;$6=1' 	| rtrace -I -ab 0 -h -oov first_envelope/envelope.oct 	| rcalc -e '$1=$1;$2=$2;$3=179*(.265*$4+.670*$5+.065*$6)' 	> first_envelope/ab0_grid10illuminance.pts
-
